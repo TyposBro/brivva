0d0113135ee42de122873bb3b05acb0dd290f289
typosbro <typosbro@proton.me>
2026-03-15 00:31:59 +0900

    Phase 2 is done

--- rust-learn/translate-cli/src/main.rs
+++ rust-learn/translate-cli/src/main.rs
@@ -1,31 +1,43 @@
+use reqwest::header::HeaderMap;
+use serde::{Deserialize, Serialize, ser};
 use std::fmt;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args: Vec<String> = std::env::args().collect();
-    // let text: String = match parse_language(&args[1]) {
-    //     Ok(lang) => println!("Parsed: {}", lang),
-    //     Err(e) => {
-    //         println!("Error: {}", e)
-    //     }
-    // };
 
-    let req = TranslationRequest {
-        text: args[1].clone(),
-        source_lang: Language::English,
-        target_lang: Language::Japanese,
-    };
+    let res = translate(&args[1], Language::English, Language::Japanese, &args[2]).await;
 
-    println!("{}", req.describe());
+    println!("{:?}", res);
 }
 
-#[derive(Debug)]
+#[derive(Deserialize, Debug)]
+struct TranslationResponse {
+    success: bool,
+    result: TranslatedText,
+}
+
+#[derive(Deserialize, Debug)]
+struct TranslatedText {
+    translated_text: String,
+}
+
+#[derive(Debug, Serialize)]
 enum Language {
+    #[serde(rename = "en")]
     English,
+
+    #[serde(rename = "ja")]
     Japanese,
+
+    #[serde(rename = "zh")]
     Chinese,
+
+    #[serde(rename = "ko")]
     Korean,
 }
 
+#[derive(Serialize, Debug)]
 struct TranslationRequest {
     text: String,
     source_lang: Language,
@@ -51,6 +63,47 @@ impl TranslationRequest {
             self.text, self.source_lang, self.target_lang
         )
     }
+}
+
+#[derive(Debug)]
+enum TranslationError {
+    Http(reqwest::Error),
+    InvalidLanguage(String),
+}
+
+impl From<reqwest::Error> for TranslationError {
+    fn from(value: reqwest::Error) -> Self {
+        TranslationError::Http(value)
+    }
+}
+
+async fn translate(
+    text: &str,
+    source: Language,
+    target: Language,
+    token: &str,
+) -> Result<String, TranslationError> {
+    let req = TranslationRequest {
+        text: text.to_string(),
+        source_lang: source,
+        target_lang: target,
+    };
+
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        "Authorization",
+        format!("Bearer {}", token).parse().unwrap(),
+    );
+    let client = reqwest::Client::new();
+    let res = client.post("https://api.cloudflare.com/client/v4/accounts/80a55132ae169d5b282ccf505bc66bf7/ai/run/@cf/meta/m2m100-1.2b")
+    .headers(headers)
+    .json(&req)
+    .send()
+    .await?
+    .json::<TranslationResponse>()
+    .await?;
+
+    Ok(res.result.translated_text)
 }
 
 fn parse_language(s: &str) -> Result<Language, String> {
